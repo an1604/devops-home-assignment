7763d12f0ea1717ba7bc4fed798b42d69edddb89
an1604 <nataf12386@cs.colman.ac.il>
2025-05-25 12:28:20 +0300

    feat(terraform): add NAT Gateway and Elastic IP resources

---
+++ nat-gateway.tf
@@ -0,0 +1,20 @@
+# Elastic IP for NAT Gateway
+resource "aws_eip" "moveo_eip" {
+    vpc = true
+
+    tags = {
+        Name = "${var.environment}-eip"
+    }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "moveo_nat" {
+    allocation_id = aws_eip.moveo_eip.id
+    depends_on = [ aws_eip.moveo_eip ] # ensuring EIP is created before NAT Gateway
+    subnet_id = aws_subnet.moveo_public_subnet.id
+    
+    tags = {
+        Name = "${var.environment}-nat"
+    }
+}
+
