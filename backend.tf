5f5701bee262b026fdf4505a512b7bff7d9403e5
an1604 <nataf12386@cs.colman.ac.il>
2025-05-27 17:42:32 +0300

    refactor(terraform): update S3 backend configuration
    - Modified the `backend.tf` file to replace the `use_lockfile` setting with `dynamodb_table` for state locking, enhancing the management of Terraform state.
    - This change aligns with best practices for state management in Terraform, ensuring improved reliability and security.

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "moveo-terraform-state-2024"
     key            = "terraform.tfstate"
-    region         = "us-east-1"  
-    use_lockfile   = true
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
 }
