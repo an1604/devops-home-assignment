683ded24a2d582364c427823dc346bc44240854c
an1604 <nataf12386@cs.colman.ac.il>
2025-05-27 17:41:23 +0300

    feat(terraform): add backend configuration and cleanup
    - Introduced a new `backend.tf` file to configure S3 as the backend for Terraform state management, ensuring state files are securely stored and versioned.
    - Added a `main.tf` file to define AWS resources for the S3 bucket and DynamoDB table for state locking.
    - Updated `.gitignore` to include `cleanup.sh` for better project maintenance.
    - Removed the deprecated `terraform.tfvars.example` file to streamline configuration management.

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "moveo-terraform-state-2024"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"  
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
+
